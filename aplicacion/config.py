c55ccd96819a6d8c675f2927fc1aef8878224920
davidenrique <davidenriquea@icloud.com>
2025-11-13 19:18:28 +0100

    Subiendo contenido al repo

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -6,10 +6,8 @@ secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)	
 
 DEBUG = True
-# Configuración de la Base de Datos MySQL de PythonAnywhere
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ovifernandez:[TU_PASSWORD_DE_MYSQL]@ovifernandez.mysql.pythonanywhere-services.com/ovifernandez$tienda'
 
 # --- EL RESTO DE TUS CONFIGURACIONES ---
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ovifernandez:TU_PASSWORD_DE_MYSQL@ovifernandez.mysql.pythonanywhere-services.com/ovifernandez$tienda'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ovifernandez:PU@adjYjk@t^*jQe0cYU@ovifernandez.mysql.pythonanywhere-services.com/ovifernandez$tienda'
